4c1374e1fbba52ca111897dc4eeacfaf4c57b979
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-04 16:08:40 -0500

    Add shouted greetings

--- main.rb
+++ main.rb
@@ -2,3 +2,39 @@ require "pry"
 require "sinatra"
 
 # Your routes go here:
+
+#Shouted greeting
+get '/shouted-greeting/:name' do
+  "HELLO, #{params['name'].upcase}"
+end
+
+get '/shouted-greeting/howdy/:name' do
+  "HOWDY, #{params['name'].upcase}"
+end
+
+
+=begin
+
+get '/hello/:name' do
+  "Hello, #{params['name'].capitalize}"
+end
+
+get '/hello/:name/:greeting' do
+  "#{params['greeting']} #{params['name']}"
+end
+
+get '/letter-count/:word' do
+  binding.pry
+  "#{params['word']} has #{params['word'].length} letters"
+end
+
+# /letter-count-queries?word=ruby
+get '/letter-count-queries' do
+  binding.pry
+  if params['word']
+    "#{params['word']} has #{params['word'].length} letters"
+  else
+    "You gotta give us a word."
+  end
+end
+=end
